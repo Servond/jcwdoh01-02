8b38d94dca311281804d6c45518d909831e38bf0
bryan karta <karta.bryan@gmail.com>
2025-07-10 10:36:59 +0700

    add cors

--- module-03/rest-api/dist/index.js
+++ module-03/rest-api/dist/index.js
@@ -4,14 +4,16 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
 // ROUTER
 const expense_route_1 = __importDefault(require("./routes/expense.route"));
 const PORT = 8080;
 const app = (0, express_1.default)();
 // MIDDLEWARE
+app.use((0, cors_1.default)());
 app.use(express_1.default.json());
 // ROUTE
-app.use("/expenses", expense_route_1.default);
+app.use("/api/expenses", expense_route_1.default);
 // ERROR HANDLING MIDDLEWARE
 app.use((err, req, res, next) => {
     res.status(500).send(err === null || err === void 0 ? void 0 : err.message);
--- module-03/rest-api/src/index.ts
+++ module-03/rest-api/src/index.ts
@@ -1,5 +1,5 @@
 import express, { Request, Response, NextFunction } from "express";
-
+import cors from "cors";
 // ROUTER
 import expenseRoute from "./routes/expense.route";
 
@@ -8,10 +8,11 @@ const PORT = 8080;
 const app = express();
 
 // MIDDLEWARE
+app.use(cors());
 app.use(express.json());
 
 // ROUTE
-app.use("/expenses", expenseRoute);
+app.use("/api/expenses", expenseRoute);
 
 // ERROR HANDLING MIDDLEWARE
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
